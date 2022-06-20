ec716fbd236f147c0821c307199eca4271e3cd5d
sampath <sampathchodipilli@gmail.com>
2022-06-20 11:53:12 +0530

    Added update todo endpoint

--- src/main/java/com/app/todo/controller/TodoController.java
+++ src/main/java/com/app/todo/controller/TodoController.java
@@ -2,12 +2,14 @@ package com.app.todo.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +53,28 @@ public class TodoController {
 			response = new Response(409, "An Error Occured ! - "+e.getLocalizedMessage());
 		}
 		return new ResponseEntity<>(response,HttpStatus.OK);
+	}
+	
+	@GetMapping("/updateTodo")
+	public ResponseEntity<Response> updateTodo(@RequestParam("id") Integer id, @RequestParam("name") String name) {
+		Response response = new Response(200, "Data Updated !");
+		try {
+			Optional<Todo> byId = repository.findById(id);
+			if(!ObjectUtils.isEmpty(byId)) {
+				Todo todo = byId.get();
+				todo.setName(name);
+				todo.setLastUpdBy("todo-app");
+				todo.setLastUpdDate(new Date());
+				repository.save(todo);
+			} else {
+				logger.info("nothing to update !");
+			}
+			
+		} catch (Exception e) {
+			logger.error("",e);
+			response = new Response(400, "An Error Occured !");
+		}
+		return new ResponseEntity<Response>(response,HttpStatus.OK);
 	}
 	
 	@PostMapping("/deleteTodo")
