fecabca74f881dafa65efb701d9496e4ea86abd2
talktoacomputer <saiavinash.147@gmail.com>
2022-06-21 10:17:54 +0530

    active and inactive todos separated in list

--- react-app/src/components/TodoList.jsx
+++ react-app/src/components/TodoList.jsx
@@ -9,9 +9,20 @@ const TodoList = () => {
     <>
       <h2 style={{ margin: "10px", marginTop: "30px" }}>Tasks to do</h2>
       <div style={{ textAlign: "start" }}>
-        {todoList?.map((todo, index) => (
-          <Todo key={index} data={todo} />
-        ))}
+        {todoList
+          ?.filter((t) => t.active === true)
+          ?.map((todo, index) => (
+            <Todo key={index} data={todo} />
+          ))}
+      </div>
+      <hr />
+      <h2 style={{ margin: "10px", marginTop: "30px" }}>Completed tasks</h2>
+      <div style={{ textAlign: "start" }}>
+        {todoList
+          ?.filter((t) => t.active === false)
+          ?.map((todo, index) => (
+            <Todo key={index} data={todo} />
+          ))}
       </div>
     </>
   );
