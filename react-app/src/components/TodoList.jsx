09caf4956da9288dbcaf8e2b9fa73a9731e4a4ab
talktoacomputer <saiavinash.147@gmail.com>
2022-06-22 16:45:58 +0530

    added conditional render for completed tasks

--- react-app/src/components/TodoList.jsx
+++ react-app/src/components/TodoList.jsx
@@ -5,26 +5,38 @@ import Todo from "./Todo";
 
 const TodoList = () => {
   const { todos: todoList } = useContext(TodoContext);
+  const remaining = todoList?.filter((t) => t.active === true);
+  const completed = todoList?.filter((t) => t.active === false);
 
   return (
     <CenterLayout>
       <h2 className="h3">Tasks to do</h2>
       <div>
-        {todoList
-          ?.filter((t) => t.active === true)
-          ?.map((todo, index) => (
-            <Todo key={index} data={todo} />
-          ))}
+        {remaining?.length === 0
+          ? "No tasks pending. Add some to keep track!"
+          : ""}
+        {remaining?.map((todo, index) => (
+          <Todo key={index} data={todo} />
+        ))}
       </div>
-      <hr className="my-4" />
-      <h2 className="h3">Completed tasks</h2>
-      <div>
-        {todoList
-          ?.filter((t) => t.active === false)
-          ?.map((todo, index) => (
-            <Todo key={index} data={todo} />
-          ))}
-      </div>
+      {completed?.length > 0 ? (
+        <>
+          <hr className="my-4" />
+          <h2 className="h3">Completed tasks</h2>
+          <div className="mb-4">
+            {remaining?.length === 0
+              ? "All pending tasks completed. Good work!🎉"
+              : ""}
+          </div>
+          <div>
+            {completed?.map((todo, index) => (
+              <Todo key={index} data={todo} />
+            ))}
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </CenterLayout>
   );
 };
