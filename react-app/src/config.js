f2c2ef242cc1e0e86207f1afa4183905bbaa3534
talktoacomputer <saiavinash.147@gmail.com>
2022-06-23 16:47:11 +0530

    added api url to .env

--- react-app/src/config.js
+++ react-app/src/config.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+const config = {
+  baseURL:
+    process.env.REACT_APP_BASE_API_URL ?? "http://localhost:8080/todo-app/",
+};
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/todo-app/",
+  baseURL: config.baseURL,
 });
+
+export default config;
